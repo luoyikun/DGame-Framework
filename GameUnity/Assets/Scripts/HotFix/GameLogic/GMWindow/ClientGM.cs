f647d0b4ba4b728a2ec57676e7068a0f662aa85a
Rock <799375868@qq.com>
2026-01-17 01:31:03 +0800

    GMWindow

--- GameUnity/Assets/Scripts/HotFix/GameLogic/GMWindow/ClientGM.cs
+++ GameUnity/Assets/Scripts/HotFix/GameLogic/GMWindow/ClientGM.cs
@@ -1,7 +1,99 @@
-﻿namespace GameLogic
+﻿using System;
+using System.Collections.Generic;
+using DGame;
+using UnityEngine;
+
+namespace GameLogic
 {
-    public class ClientGM
+    public delegate void HandleGM(List<string> @params);
+
+    public class ClientGM : Singleton<ClientGM>, IUpdate
     {
-        
+        private readonly Dictionary<string, HandleGM> m_cmdDict = new Dictionary<string, HandleGM>();
+
+        /// <summary>
+        /// 缓存输入过的命令
+        /// </summary>
+        private readonly List<string> m_commendList = new List<string>();
+
+        private readonly string[] m_gmTextSplitArr = new[] { " " };
+
+        private GmCmdHandler m_cmdHandler;
+
+        public bool debugLowFps = false;
+        public bool debugNotchScreen = false;
+
+        protected override void OnInit()
+        {
+            m_cmdHandler = new GmCmdHandler();
+            m_cmdHandler.Init();
+        }
+
+        public void HandleClientGm(string gmText)
+        {
+            gmText = gmText.Trim();
+            DLogger.Info($"type: {gmText.GetType().FullName}");
+            string[] strArr = gmText.Split(m_gmTextSplitArr, Int32.MaxValue, StringSplitOptions.RemoveEmptyEntries);
+            DLogger.Info($"split array length: {strArr.Length}");
+            List<string> gmParams = new List<string>();
+            string cmd = strArr[0];
+
+            for (int i = 1; i < strArr.Length; i++)
+            {
+                string param1 = strArr[i].Trim();
+
+                if (string.IsNullOrEmpty(param1))
+                {
+                    continue;
+                }
+                gmParams.Add(param1);
+            }
+            DLogger.Warning($"ClientGM: {gmText} param count: {gmParams.Count}");
+
+            if (m_cmdDict.TryGetValue(cmd, out HandleGM handler))
+            {
+                handler(gmParams);
+            }
+        }
+
+        public void RegisterGmCmd(string cmd, HandleGM handler)
+        {
+            if (!m_cmdDict.TryAdd(cmd, handler))
+            {
+                DLogger.Error($"repeat GmCmd: {cmd}");
+            }
+        }
+
+        public bool TryGetCommendByIndex(int index, out string commend)
+        {
+            bool inRange = false;
+            commend = string.Empty;
+
+            if (index >= 0 && index < m_commendList.Count)
+            {
+                inRange = true;
+                commend = m_commendList[index];
+            }
+
+            if (index >= m_commendList.Count && m_commendList.Count > 0)
+            {
+                commend = m_commendList[0];
+                inRange = false;
+            }
+            return inRange;
+        }
+
+        public void AddCommend(string commend)
+        {
+            m_commendList.Insert(0, commend);
+        }
+
+        public void OnUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.F1))
+            {
+                // UIModule.Instance.ShowWindowAsync<GMWindow>();
+            }
+        }
     }
 }
--- GameUnity/Assets/Scripts/HotFix/GameLogic/GMWindow/GmCmdHandler.cs
+++ GameUnity/Assets/Scripts/HotFix/GameLogic/GMWindow/GmCmdHandler.cs
@@ -1,7 +1,30 @@
-﻿namespace GameLogic
+﻿using System.Collections.Generic;
+using DGame;
+
+namespace GameLogic
 {
     public class GmCmdHandler
     {
-        
+        public void Init()
+        {
+            RegisterGmCmd("gc", Gc);
+        }
+
+        public void RegisterGmCmd(string cmd, HandleGM func)
+        {
+            ClientGM.Instance.RegisterGmCmd(cmd, func);
+        }
+
+        public void ShowText(string format, params object[] args)
+        {
+            DLogger.Info(format, args);
+            string retStr = string.Format(format, args);
+            // UIModule.Instance.ShowWindowAsync<GMWindow>(retStr);
+        }
+
+        private void Gc(List<string> paras)
+        {
+            GameModule.ResourceModule.ForceUnloadUnusedAssets(true);
+        }
     }
 }
