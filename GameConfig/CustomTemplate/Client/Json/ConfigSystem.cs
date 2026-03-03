d7479530bf5ae2e52887896e0cd13635a6f3e356
Jun <799375868@qq.com>
2026-03-03 17:13:59 +0800

    配置表模板更新

--- GameConfig/CustomTemplate/Client/Json/ConfigSystem.cs
+++ GameConfig/CustomTemplate/Client/Json/ConfigSystem.cs
@@ -4,55 +4,58 @@ using DGame;
 using SimpleJSON;
 using UnityEngine;
 
-/// <summary>
-/// 配置加载器。
-/// </summary>
-public class ConfigSystem
+namespace GameProto
 {
-    private static ConfigSystem m_instance;
-
-    public static ConfigSystem Instance => m_instance != null ? m_instance : m_instance = new ConfigSystem();
-
-    private bool m_init = false;
-
-    private Tables m_tables;
-
-    public Tables Tables
+    /// <summary>
+    /// 配置加载器。
+    /// </summary>
+    public class ConfigSystem
     {
-        get
+        private static ConfigSystem m_instance;
+
+        public static ConfigSystem Instance => m_instance != null ? m_instance : m_instance = new ConfigSystem();
+
+        private bool m_init = false;
+
+        private Tables m_tables;
+
+        public Tables Tables
         {
-            if (!m_init)
+            get
             {
-                Load();
+                if (!m_init)
+                {
+                    Load();
+                }
+
+                return m_tables;
             }
-
-            return m_tables;
         }
-    }
 
-    private IResourceModule m_resourceModule;
+        private IResourceModule m_resourceModule;
 
-    /// <summary>
-    /// 加载配置。
-    /// </summary>
-    public void Load()
-    {
-        m_tables = new Tables(LoadJsonNode);
-        m_init = true;
-    }
-
-    /// <summary>
-    /// 加载Json配置。
-    /// </summary>
-    /// <param name="file">FileName</param>
-    /// <returns>ByteBuf</returns>
-    private JSONNode LoadJsonNode(string file)
-    {
-        if (m_resourceModule == null)
+        /// <summary>
+        /// 加载配置。
+        /// </summary>
+        public void Load()
         {
-            m_resourceModule = ModuleSystem.GetModule<IResourceModule>();
+            m_tables = new Tables(LoadJsonNode);
+            m_init = true;
         }
-        TextAsset textAsset = m_resourceModule.LoadAsset<TextAsset>(file);
-        return JsonUtility.FromJson<JSONNode>(textAsset.text);
+
+        /// <summary>
+        /// 加载Json配置。
+        /// </summary>
+        /// <param name="file">FileName</param>
+        /// <returns>ByteBuf</returns>
+        private JSONNode LoadJsonNode(string file)
+        {
+            if (m_resourceModule == null)
+            {
+                m_resourceModule = ModuleSystem.GetModule<IResourceModule>();
+            }
+            TextAsset textAsset = m_resourceModule.LoadAsset<TextAsset>(file);
+            return JsonUtility.FromJson<JSONNode>(textAsset.text);
+        }
     }
 }
--- GameConfig/CustomTemplate/Server/Json/ConfigSystem.cs
+++ GameConfig/CustomTemplate/Server/Json/ConfigSystem.cs
@@ -1,59 +1,62 @@
 using Luban;
 using SimpleJSON;
 
-/// <summary>
-/// 配置加载器。
-/// </summary>
-public class ConfigSystem
+namespace GameProto
 {
-    private static ConfigSystem m_instance;
-
-    public static ConfigSystem Instance => m_instance != null ? m_instance : m_instance = new ConfigSystem();
-
-    private bool m_init = false;
-
-    private Tables m_tables;
-
-    public Tables Tables
+    /// <summary>
+    /// 配置加载器。
+    /// </summary>
+    public class ConfigSystem
     {
-        get
+        private static ConfigSystem m_instance;
+
+        public static ConfigSystem Instance => m_instance != null ? m_instance : m_instance = new ConfigSystem();
+
+        private bool m_init = false;
+
+        private Tables m_tables;
+
+        public Tables Tables
         {
-            if (!m_init)
+            get
             {
-                Load();
+                if (!m_init)
+                {
+                    Load();
+                }
+
+                return m_tables;
             }
-
-            return m_tables;
         }
-    }
 
-    /// <summary>
-    /// 加载配置。
-    /// </summary>
-    public void Load()
-    {
-        m_tables = new Tables(LoadJsonNode);
-        m_init = true;
-    }
+        /// <summary>
+        /// 加载配置。
+        /// </summary>
+        public void Load()
+        {
+            m_tables = new Tables(LoadJsonNode);
+            m_init = true;
+        }
 
-    /// <summary>
-    /// 加载Json配置。
-    /// </summary>
-    /// <param name="file">FileName</param>
-    /// <returns>ByteBuf</returns>
-    private JSONNode LoadJsonNode(string file)
-    {
-        // 在这里编写服务器加载配置的逻辑
-        var configPath = GenerateConfigPath(file);
-        var jsonText = File.ReadAllText(configPath);
-        return JSONNode.Parse(jsonText);
-    }
+        /// <summary>
+        /// 加载Json配置。
+        /// </summary>
+        /// <param name="file">FileName</param>
+        /// <returns>ByteBuf</returns>
+        private JSONNode LoadJsonNode(string file)
+        {
+            // 在这里编写服务器加载配置的逻辑
+            var configPath = GenerateConfigPath(file);
+            var jsonText = File.ReadAllText(configPath);
+            return JSONNode.Parse(jsonText);
+        }
 
-    /// <summary>
-    /// 生成配置表存放路径。
-    /// </summary>
-    /// <param name="file">FileName</param>
-    /// <returns>configPath</returns>
-    private string GenerateConfigPath(string file)
-        => $"{AppContext.BaseDirectory}/../../../../../Configs/Json/{file}.json";
+        /// <summary>
+        /// 生成配置表存放路径。
+        /// </summary>
+        /// <param name="file">FileName</param>
+        /// <returns>configPath</returns>
+        private string GenerateConfigPath(string file)
+            => $"{AppContext.BaseDirectory}/../../../../../Configs/Json/{file}.json";
+    }
 }
